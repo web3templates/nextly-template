b6807cc4abfded49345bc50c0f55b130e5c5622a
didiermunezero <didiermunezer38@gmail.com>
2021-06-11 20:05:22 +0200

    fix: display focused item in dark mode in a different color

--- components/navbar.js
+++ components/navbar.js
@@ -55,7 +55,7 @@ export default function Navbar() {
                   <>
                     {navigation.map((item, index) => (
                       <Link key={index} href="/">
-                        <a className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-trueGray-700">
+                        <a className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-trueGray-700 dark:focus:text-black">
                           {item}
                         </a>
                       </Link>
@@ -78,7 +78,7 @@ export default function Navbar() {
             {navigation.map((menu, index) => (
               <li className="mr-3 nav__item" key={index}>
                 <Link href="/">
-                  <a className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none">
+                  <a className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 dark:focus:text-black hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none">
                     {menu}
                   </a>
                 </Link>
