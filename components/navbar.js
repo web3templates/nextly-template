1e0ecfd1919fce500cde28419c4d0b043d78c00b
Surjith S M <surjithctly@gmail.com>
2021-09-06 20:39:53 +0530

    fix dark menu focs

--- components/navbar.js
+++ components/navbar.js
@@ -84,7 +84,7 @@ export default function Navbar() {
             {navigation.map((menu, index) => (
               <li className="mr-3 nav__item" key={index}>
                 <Link href="/">
-                  <a className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none">
+                  <a className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
                     {menu}
                   </a>
                 </Link>
